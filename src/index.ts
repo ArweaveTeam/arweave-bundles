3fff304b7907a7c416badb9b790f6438ea23c716
aidanok <52333119+aidanok@users.noreply.github.com>
2020-04-08 14:59:46 +0100

    add helpers to bundle unbundle datas

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Dependencies } from './ar-data-base';
 import { getSignatureData, DataItemJson } from './ar-data-base';
 import { createData, sign, addTag, DataItemCreateOptions } from './ar-data-create';
 import { decodeData, decodeTag, decodeTagAt, unpackTags } from './ar-data-read';
+import { bundleData, unbundleData } from './ar-data-bundle'
 import { verify  } from './ar-data-verify';
 
 export { createData as create, sign, decodeData, decodeTag, decodeTagAt, unpackTags, verify, DataItemCreateOptions, DataItemJson, getSignatureData }
@@ -15,7 +16,9 @@ export function ArDataApi(deps: Dependencies) {
     verify: verify.bind(null, deps),
     decodeData: decodeData.bind(null, deps),
     decodeTag: decodeTag.bind(null, deps),
-    decodeTagAt: decodeTagAt.bind(null, deps), 
+    decodeTagAt: decodeTagAt.bind(null, deps),
     unpackTags: unpackTags.bind(null, deps),
+    bundleData: bundleData.bind(null, deps),
+    unbundleData: unbundleData.bind(null, deps),
   }
 }
