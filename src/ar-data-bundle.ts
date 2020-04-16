c2bfc45c76d4a816ce86191cce5875d1db1ba892
aidanok <52333119+aidanok@users.noreply.github.com>
2020-04-16 10:00:22 +0100

    return empty array on invalid txbody + jsdoc

--- src/ar-data-bundle.ts
+++ src/ar-data-bundle.ts
@@ -2,13 +2,28 @@ import { DataItemJson } from ".";
 import { Dependencies } from "./ar-data-base";
 import { verify } from "./ar-data-verify";
 
-
+/**
+ * Unbundles a transaction into an Array of DataItems.
+ * 
+ * Takes either a json string or object. Will throw if given an invalid json
+ * string but otherwise, it will return an empty array if 
+ * 
+ * a) the json object is the wrong format
+ * b) the object contains no valid DataItems. 
+ * 
+ * It will verify all DataItems and discard ones that don't pass verification.
+ * 
+ * @param deps
+ * @param txData 
+ */
 export async function unbundleData(deps: Dependencies, txData: any): Promise<DataItemJson[]> {
   if (typeof txData === 'string') {
-    txData = JSON.parse(txData)
-  } 
+    txData = JSON.parse(txData); 
+  }
+
   if (typeof txData !== 'object' || !txData || !txData.items || !Array.isArray(txData.items)) {
-    throw new Error('Invalid data, should be a json string or obect with an items Array');
+    console.warn(`Invalid bundle, should be a json string or obect with an items Array`)
+    return [];
   }
 
   const itemsArray = txData.items as DataItemJson[];
